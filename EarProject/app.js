8af77b383cf0913701b5c64234a9f82d1369556f
jcgod413 <jcgod413@gmail.com>
2017-06-27 18:06:28 +0900

    main log

--- EarProject/app.js
+++ EarProject/app.js
@@ -6,6 +6,14 @@ var express = require('express');
 var path = require('path');
 var routes = require('./routes');
 
+var client = mysql.createConnection({
+    host : 'mail.chawoo.me',
+    port: '3306',
+    user: 'jaecheol',
+    database: 'earporject',
+    password: 'wocorl'
+});
+
 // 웹 서버를 생성합니다.
 var app = express();
 
@@ -16,7 +24,17 @@ app.listen(3030, () => {
   console.log(`Server is running on 3030 port`);
 });
 
-app.use('/', routes);
+// app.use('/', routes);
+
+app.get('/', function(request, response)  {
+	console.log('/////');
+    var sentence = 'SELECT * FROM User';
+	client.query(sentence, function(error, results) {
+		response.writeHead(200, {'Content-Type' : 'Text/html'});
+		response.end(results);
+		console.log(results);
+	});
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- EarProject/app/public/user.js
+++ EarProject/app/public/user.js
@@ -1,23 +1,8 @@
 var mysql = require('mysql');
 
-var client = mysql.createConnection({
-    host : 'mail.chawoo.me',
-    port: '3306',
-    user: 'jaecheol',
-    database: 'earporject',
-    password: 'wocorl'
-});
-
 module.exports = {
     login(req, res) {
         console.log(`Login Request`);
-
-        var sentence = 'SELECT * FROM User';
-        client.query(sentence, function(error, results) {
-            response.writeHead(200, {'Content-Type' : 'Text/html'});
-            response.end(results);
-            console.log(results);
-        })
     },
 
     verify(req, res) {
