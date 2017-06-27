537b340cae85b129638d8862bbbe219d64c74bc3
jcgod413 <jcgod413@gmail.com>
2017-06-27 17:40:32 +0900

    db test

--- EarProject/app/public/user.js
+++ EarProject/app/public/user.js
@@ -1,6 +1,20 @@
+var mysql = require('mysql');
+
+var client = mysql.createConnection({
+    host : 'mail.chawoo.me',
+    port: '3306',
+    user: 'jaecheol',
+    database: 'earporject',
+    password: 'wocorl'
+});
+
 module.exports = {
     login(req, res) {
-        
+        var sentence = 'SELECT * FROM User';
+        client.query(sentence, function(error, results) {
+            response.writeHead(200, {'Content-Type' : 'Text/html'});
+            response.end(results);
+        })
     },
 
     verify(req, res) {
