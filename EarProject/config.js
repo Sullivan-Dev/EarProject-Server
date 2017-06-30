8623e27f6e67efd699b3f338a2fdbc5e854af37e
jcgod413 <jcgod413@gmail.com>
2017-06-30 22:59:41 +0900

    config file delete

--- EarProject/config.js
+++
@@ -1,6 +0,0 @@
-exports.MYSQL_HOST = 'mail.chawoo.me';
-exports.MYSQL_USER = 'jaecheol';
-exports.MYSQL_PASSWORD = 'wocorl';
-exports.MYSQL_DB = 'earproject';
-
-exports.JWT_TOKEN = 'sullivanjwttoken';
